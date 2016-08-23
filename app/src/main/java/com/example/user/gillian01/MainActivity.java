5b6dcad4f6d854b947113f1e50ac232d18d26fd9
jjjgalliano <ginaginayang@gmail.com>
2016-08-23 17:02:14 +0800

    finish MIanActivity UI add button

--- app/src/main/java/com/example/user/gillian01/MainActivity.java
+++ app/src/main/java/com/example/user/gillian01/MainActivity.java
@@ -3,14 +3,34 @@ package com.example.user.gillian01;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.*;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    TextView infoText;
+    EditText nameEditText;
+    RadioGroup optionGroup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Log.d("Testlog","hello");
-        Log.d("Testlog","hello2");
+        setContentView(R.layout.activity_new_main); //enter
+
+        infoText =(TextView)findViewById(R.id.infoText);
+        nameEditText =(EditText)findViewById(R.id.nameEditText);
+        optionGroup =(RadioGroup)findViewById(R.id.optionGroup);
+        Button confirmBtn = (Button)findViewById(R.id.confirmbutton);
+        confirmBtn.setOnClickListener(MainActivity.this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        int viewId = v.getId();
+        if(viewId == R.id.confirmbutton)
+        {
+            //confirm
+            Log.d("buttonTest","fonfirm button was clicked");
+        }
     }
 }
