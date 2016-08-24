e6dd8abbe8b309b97f822ecf5dca2bcce36bacbd
jjjgalliano <jjjgalliano@gmail.com>
2016-08-24 15:21:24 +0800

    input
    Day3 input

--- app/src/main/java/com/example/user/gillian01/MainActivity.java
+++ app/src/main/java/com/example/user/gillian01/MainActivity.java
@@ -1,17 +1,23 @@
 package com.example.user.gillian01;
 
+import android.content.Context;
+import android.hardware.input.InputManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.*;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+public class MainActivity extends AppCompatActivity implements View.OnClickListener,EditText.OnEditorActionListener{
 
     static final String[] pokemonNames ={"傑尼龜","妙蛙種子","可達鴨"};
     TextView infoText;
     EditText nameEditText;
     RadioGroup optionGroup;
+    Button confirmBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,8 +26,11 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         infoText =(TextView)findViewById(R.id.infoText);
         nameEditText =(EditText)findViewById(R.id.nameEditText);
+        nameEditText.setOnEditorActionListener(MainActivity.this);
+        nameEditText.setImeOptions(EditorInfo.IME_ACTION_DONE);
+
         optionGroup =(RadioGroup)findViewById(R.id.optionGroup);
-        Button confirmBtn = (Button)findViewById(R.id.confirmbutton);
+        confirmBtn = (Button)findViewById(R.id.confirmbutton);
         confirmBtn.setOnClickListener(MainActivity.this);
     }
 
@@ -37,16 +46,33 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
             int  selectedradioButtonID = optionGroup.getCheckedRadioButtonId();
            View selectedRadioButtonView = optionGroup.findViewById(selectedradioButtonID);
-            int selectedIndex = optionGroup.indexOfChild(selectedRadioButtonView);
+           // int selectedIndex = optionGroup.indexOfChild(selectedRadioButtonView);
 
             RadioButton selectedRadioButton =(RadioButton)selectedRadioButtonView;
             String radionBtnText = selectedRadioButton.getText().toString();
 
-            String welcomemessages = String.format("你好,訓練者 %s ,你的第一個夥伴是%s",name,radionBtnText );
+            String welcomemessages = String.format("你好,訓練者 %s ,歡迎你來到這個世界,你的第一個夥伴是%s",name,radionBtnText );
             infoText.setText(welcomemessages);
 
         }
 
 
+    }
+
+    @Override
+    public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+        if(actionId == EditorInfo.IME_ACTION_DONE)
+        {
+            InputMethodManager inm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+            // setup android setting > keyboard language > current keyboard > hardware  show method keyboard > on
+            inm.hideSoftInputFromWindow(v.getWindowToken(), 0);
+
+            //simulate button clicked
+            confirmBtn.performClick();
+            return true;
+
+        }
+
+        return false;
     }
 }
---
+++ app/src/main/java/com/example/user/gillian01/model/OwnedPokemonInfo.java
@@ -0,0 +1,15 @@
+package com.example.user.gillian01.model;
+
+/**
+ * Created by user on 2016/8/24.
+ */
+public class OwnedPokemonInfo {
+
+    public int pokemonId;
+    public String name;
+    public int level;
+    public int currentHP;
+    public int maxHP;
+
+
+}
